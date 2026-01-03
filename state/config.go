4491f025b703eb57877d3a8cf37f8094ec7dc321
Crash Developer <developer@cococode.work>
2026-01-03 19:07:51 +0200

    All my new config that I want to preserve

--- state/config.go
+++ state/config.go
@@ -3,9 +3,9 @@ package state
 import (
 	"fmt"
 	"io"
-	"net/url"
 	"os"
 
+	"github.com/PaulSonOfLars/gotgbot/v2"
 	"gopkg.in/yaml.v3"
 )
 
@@ -13,31 +13,27 @@ type Config struct {
 	Path             string `yaml:"-"`
 	TimeZone         string `yaml:"time_zone"`
 	TimeFormat       string `yaml:"time_format"`
-	GitExecutable    string `yaml:"git_executable"`
-	GoExecutable     string `yaml:"go_executable"`
+	MediaTimeFormat  string `yaml:"media_time_format"`
 	FfmpegExecutable string `yaml:"ffmpeg_executable"`
 	DebugMode        bool   `yaml:"debug_mode"`
-
-	UseGithHubBinaries bool   `yaml:"use_github_binaries"`
-	Architecture       string `yaml:"architecture"`
+	SilentDbLogs     bool   `yaml:"silent_db_logs"`
 
 	Telegram struct {
-		BotToken            string  `yaml:"bot_token"`
-		APIURL              string  `yaml:"api_url"`
-		SudoUsersID         []int64 `yaml:"sudo_users_id"`
-		OwnerID             int64   `yaml:"owner_id"`
-		TargetChatID        int64   `yaml:"target_chat_id"`
-		SelfHostedAPI       bool    `yaml:"self_hosted_api"`
-		SkipVideoStickers   bool    `yaml:"skip_video_stickers"`
-		SkipSettingCommands bool    `yaml:"skip_setting_commands"`
-		SendMyPresence      bool    `yaml:"send_my_presence"`
-		SendMyReadReceipts  bool    `yaml:"send_my_read_receipts"`
-		SilentConfirmation  bool    `yaml:"silent_confirmation"`
-		ConfirmationType    string  `yaml:"confirmation_type"`
-		EmojiConfirmation   *bool   `yaml:"emoji_confirmation"`
-		SkipStartupMessage  bool    `yaml:"skip_startup_message"`
-		SpoilerViewOnce     bool    `yaml:"spoiler_as_viewonce"`
-		Reactions           bool    `yaml:"reactions"`
+		BotToken                string  `yaml:"bot_token"`
+		ApiUrl                  string  `yaml:"api_url"`
+		SudoUsersID             []int64 `yaml:"sudo_users_id"`
+		OwnerID                 int64   `yaml:"owner_id"`
+		TargetChatID            int64   `yaml:"target_chat_id"`
+		SelfHostedAPI           bool    `yaml:"self_hosted_api"`
+		SkipVideoStickers       bool    `yaml:"skip_video_stickers"`
+		RemoveBotCommands       bool    `yaml:"remove_bot_commands"`
+		SendMyPresenceOnReply   bool    `yaml:"send_my_presence_on_reply_on_reply"`
+		SendReadReceiptsOnReply bool    `yaml:"send_read_receipts_on_reply"`
+		SilentConfirmation      bool    `yaml:"silent_confirmation"`
+		ConfirmationType        string  `yaml:"confirmation_type"`
+		SkipStartupMessage      bool    `yaml:"skip_startup_message"`
+		SpoilerViewOnce         bool    `yaml:"spoiler_as_viewonce"`
+		Reactions               bool    `yaml:"reactions"`
 	} `yaml:"telegram"`
 
 	WhatsApp struct {
@@ -50,6 +46,7 @@ type Config struct {
 			AuthorName string `yaml:"author_name"`
 		} `yaml:"sticker_metadata"`
 		SessionName                    string   `yaml:"session_name"`
+		BrowserName                    string   `yaml:"browser_name"`
 		TagAllAllowedGroups            []string `yaml:"tag_all_allowed_groups"`
 		IgnoreChats                    []string `yaml:"ignore_chats"`
 		StatusIgnoredChats             []string `yaml:"status_ignored_chats"`
@@ -65,11 +62,17 @@ type Config struct {
 		SkipLocations                  bool     `yaml:"skip_locations"`
 		SkipProfilePictureUpdates      bool     `yaml:"skip_profile_picture_updates"`
 		SkipGroupSettingsUpdates       bool     `yaml:"skip_group_settings_updates"`
+		SkipUserAboutUpdates           bool     `yaml:"skip_user_about_updates"`
 		SkipChatDetails                bool     `yaml:"skip_chat_details"`
-		SendRevokedMessageUpdates      bool     `yaml:"send_revoked_message_updates"`
+		SkipRevokedMessage             bool     `yaml:"skip_revoked_message"`
 		WhatsmeowDebugMode             bool     `yaml:"whatsmeow_debug_mode"`
 		SendMyMessagesFromOtherDevices bool     `yaml:"send_my_messages_from_other_devices"`
 		CreateThreadForInfoUpdates     bool     `yaml:"create_thread_for_info_updates"`
+		AllowEveryoneTagging           bool     `yaml:"allow_everyone_tagging"`
+		SkipStartupMessage             bool     `yaml:"skip_startup_message"`
+		SkipQrCodeSend                 bool     `yaml:"skip_qr_code"`
+		SkipInitialPhotoSend           bool     `yaml:"skip_initial_photo_send"`
+		SkipInitialSync                bool     `yaml:"skip_initial_sync"`
 	} `yaml:"whatsapp"`
 
 	Database map[string]string `yaml:"database"`
@@ -98,44 +101,12 @@ func (cfg *Config) LoadConfig() error {
 		return fmt.Errorf("could not parse config file : %s", err)
 	}
 
-	whatsappLoginDB := cfg.WhatsApp.LoginDatabase
-	if whatsappLoginDB.Type == "sqlite3" {
-		parsedUrl, err := url.Parse(whatsappLoginDB.URL)
-		if err != nil {
-			return fmt.Errorf("whatsapp Login Database URL is not a valid URL")
+	deprecatedOptions := GetDeprecatedConfigOptions(cfg)
+	if deprecatedOptions != nil {
+		fmt.Println("The following options have been deprecated/removed:")
+		for num, opt := range deprecatedOptions {
+			fmt.Printf("%d. %s: %s\n", num+1, opt.Name, opt.Description)
 		}
-		if _, found := parsedUrl.Query()["_foreign_keys"]; !found {
-			q := parsedUrl.Query()
-			q.Set("_foreign_keys", "on")
-			if q.Has("foreign_keys") {
-				q.Del("foreign_keys")
-			}
-			parsedUrl.RawQuery = q.Encode()
-			cfg.WhatsApp.LoginDatabase.URL = parsedUrl.String()
-			cfg.SaveConfig()
-		}
-	}
-
-	return nil
-}
-
-func (cfg *Config) SaveConfig() error {
-	configFilePath := cfg.Path
-
-	configFile, err := os.Create(configFilePath)
-	if err != nil {
-		return fmt.Errorf("could not open config file : %s", err)
-	}
-	defer configFile.Close()
-
-	newConfigBody, err := yaml.Marshal(cfg)
-	if err != nil {
-		return fmt.Errorf("failed to marshal config into string : %s", err)
-	}
-
-	_, err = configFile.Write(newConfigBody)
-	if err != nil {
-		return fmt.Errorf("failed to write config file : %s", err)
 	}
 
 	return nil
@@ -144,11 +115,13 @@ func (cfg *Config) SaveConfig() error {
 func (cfg *Config) SetDefaults() {
 	cfg.TimeZone = "UTC"
 
-	cfg.WhatsApp.SessionName = "watgbridge"
+	cfg.WhatsApp.SessionName = "coco-watg"
+	cfg.WhatsApp.BrowserName = "FIREFOX"
 	cfg.WhatsApp.LoginDatabase.Type = "sqlite3"
-	cfg.WhatsApp.LoginDatabase.URL = "file:wawebstore.db?foreign_keys=on"
-	cfg.WhatsApp.StickerMetadata.PackName = "WaTgBridge"
-	cfg.WhatsApp.StickerMetadata.AuthorName = "WaTgBridge"
+	cfg.WhatsApp.LoginDatabase.URL = "file:coco_wawebstore.db?_foreign_keys=on"
+	cfg.WhatsApp.StickerMetadata.PackName = "CocoWaTgBridge"
+	cfg.WhatsApp.StickerMetadata.AuthorName = "CocoWaTgBridge"
 
+	cfg.Telegram.ApiUrl = gotgbot.DefaultAPIURL
 	cfg.Telegram.ConfirmationType = "emoji"
 }
